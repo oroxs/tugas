167c15ae6e102b4e673e431df96642780d77cfdd
unknown <mitrais@MTNB1642.mitrais.com>
2025-02-25 21:20:19 +0700

    add comment

--- tugas4.py
+++ tugas4.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
+# List of website
 browser = ['https://tiket.com', 
            'https://www.tokopedia.com', 
            'https://www.orangsiber.com',
@@ -12,10 +13,12 @@ option.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=option)
 driver.minimize_window()
 
+# Looping to get the URL and Title of the website
 for i in browser:
     driver.get(i)
     get_url = driver.current_url
     get_title = driver.title
     print(f'{get_url} - Title: {get_title} \n')
 
+# Close the browser
 driver.close()
